82700a16a7875e3890767a272fd6bdde010aafa0
xxoellie <elliesohyeon1202@gmail.com>
2021-11-06 22:46:16 +0900

    11/6 self practice

---
+++ Algorithm/backjoon/A+B-7.py
@@ -0,0 +1,4 @@
+n = int(input())
+for i in range(1,n+1) :
+    A,B = map(int,input().split())
+    print("Case #{0}:".format(i), A+B)
--- Algorithm/backjoon/구구단.py
+++ Algorithm/backjoon/구구단.py
@@ -1,3 +1,4 @@
 a = int(input())
 for i in range(1,10):
     print("{0} * {1} = {2}".format(a,i,a*i))
+    print(f"{a} * {i} = {a*i}")
